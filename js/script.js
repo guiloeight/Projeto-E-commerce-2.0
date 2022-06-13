1123a7728f53d4bd73392382bf757e59451134b3
guiloeight <guiloeight@gmail.com>
2022-06-13 10:33:33 -0300

    up

---
+++ js/script.js
@@ -0,0 +1,98 @@
+let ProdutosJson = [
+    {
+        id: 1,
+        name: 'Escudo peruano',
+        img: 'images/1.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Escudo peruano'
+      },
+      {
+        id: 2,
+        name: 'Her',
+        img: 'images/2.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Her'
+      },
+      {
+        id: 3,
+        name: 'Lisa',
+        img: 'images/3.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Lisa'
+      },
+      {
+        id: 4,
+        name: 'Lisa estudante',
+        img: 'images/4.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Lisa estudante'
+      },
+      {
+        id: 5,
+        name: 'Mickey Mágico',
+        img: 'images/5.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Mickey Mágico'
+      },
+      {
+        id: 6,
+        name: 'Vai dar tudo certo',
+        img: 'images/6.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Vai dar tudo certo'
+      },
+      {
+        id: 7,
+        name: 'Mickey',
+        img: 'images/7.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Mickey'
+      },
+      {
+        id: 8,
+        name: 'Popeye',
+        img: 'images/8.png',
+        price: [20.00, 23.00, 25.00],
+        sizes: [
+          '',
+          '',
+          ''
+        ],
+        description: 'Popeye'
+      }
+];
